77d48c14be6914e49bb049d5a9746924d29e4dfd
sa07m <127376011+sa07m@users.noreply.github.com>
2023-10-14 11:52:36 +0530

    final commit

--- database.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize')
-const sequelize = new Sequelize('test','root','36@Chinatown',{
-    dialect:'mysql',
-    host:'localhost'
-})
-
-module.exports = sequelize
--- route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const controlAttendance = require('../controllers/controller')
-const router = express.Router()
-
-router.post('/mark-attendance', controlAttendance.postAttendance)
-router.get('/get-attendance/:date1', controlAttendance.getAttendance)
-router.get('/get-report', controlAttendance.getReport)
-
-module.exports = router
